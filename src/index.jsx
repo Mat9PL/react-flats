136b8192e6004b5a08c36e315f7ce8df5d6a7986
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-07-10 12:06:21 +0200

    minor display fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
+// external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// internal modules
 import '../assets/stylesheets/application.scss';
 import App from './components/app';
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+// render an instance of the component in the DOM
+ReactDOM.render(<App />, document.querySelector('#root'));
